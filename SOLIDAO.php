6644847fd854514e7f0aecb23ca16c237462da23
Mohamedelaribi <93386582+Mohamedelaribi@users.noreply.github.com>
2022-02-03 13:48:16 +0100

    brief11

--- SOLIDAO.php
+++ SOLIDAO.php
@@ -21,14 +21,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
             <table class="lydec_table" id="lydec_table">
-            <tr>
-                <td><span>Ancien index :</span> <b><?php echo $Ancien; ?></b></td>
-                <td><span>Nouveau index :</span> <b><?php echo $Nouveau; ?></b></td>
-                <td><span>Consommation :</span> <b><?php echo $Consommation . ' Kwh'; ?></b></td>
+            <tr id="tr1">
+                <td id="td1"><span>Ancien index :</span> <b><?php echo $Ancien; ?></b></td>
+                <td id="td1"><span>Nouveau index :</span> <b><?php echo $Nouveau; ?></b></td>
+                <td id="td1"><span>Consommation :</span> <b><?php echo $Consommation . ' Kwh'; ?></b></td>
             </tr>
             <tr>
                 <td colspan="3">
